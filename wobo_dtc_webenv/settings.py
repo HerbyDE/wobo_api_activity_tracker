0cf7f11a73c3ba7667d162ada0de60318b8e4c75
Herbert <herbert.woisetschlaeger@gmail.com>
2020-12-21 21:50:38 +0100

    Update settings.py

--- wobo_dtc_webenv/settings.py
+++ wobo_dtc_webenv/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#--0gqk)u&qb^3(tck5ex3xjx@d4!-+o84_%78md*2+^_k_!pi'
+SECRET_KEY = os.environ("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
